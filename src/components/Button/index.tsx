d0d9bb12c5a3745f2874d8261c6eb1849462825d
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-05-30 20:16:08 -0300

    chore: update button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,12 +2,18 @@ import { Container } from "./styles";
 
 interface ButtonProps {
   title: string;
+  variant?: "primary" | "red" | "blue";
+  onClick?: () => void;
 }
 
-export const Button = ({ title }: ButtonProps) => {
+export const Button = ({
+  title,
+  variant = "primary",
+  onClick,
+}: ButtonProps) => {
   return (
-    <Container>
-      <button>{title}</button>
+    <Container variant={variant}>
+      <button onClick={onClick}>{title}</button>
     </Container>
   );
 };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,22 +1,28 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+interface IProps {
+  variant: "primary" | "red" | "blue";
+}
+
+export const Container = styled.div<IProps>`
   display: flex;
   flex-direction: column;
-  align-items:center;
-  padding: 0 1rem;
+  align-items: center;
+  padding: ${({ variant }) => (variant === "primary" ? "0 1rem" : "0")};
 
   button {
     border-radius: 8px;
     width: 100%;
     border: 1px solid transparent;
-    padding: 0.8rem 0;
+    padding: ${({ variant }) =>
+      variant === "primary" ? "0.8rem 0" : "0.8rem"};
     font-size: 1rem;
     font-weight: 600;
     font-family: inherit;
-    background-color: #3a506b;
+    background-color: ${({ variant }) =>
+      variant === "red" ? "#B5342C" : "#3a506b"};
     color: #fff;
     cursor: pointer;
-    transition: border-color 0.25s;
+    transition: border - color 0.25s;
   }
 `;
